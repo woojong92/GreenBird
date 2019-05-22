0bfcb6a2a2bfc9c2d3e8586447b5f3f47773b662
Woojong Kim <woojong92@gmail.com>
2019-05-22 13:33:43 +0900

    회원가입 state와 custom hook

--- FrontServer/pages/signup.js
+++ FrontServer/pages/signup.js
@@ -6,17 +6,23 @@ import { Form, Input, Checkbox,  Button} from 'antd';
 const Signup = () => {
     
     {/* 모든 Input에 스테이트와 이벤트 리스너를 만들어 줘야 한다. */}
-    
     const [id, setId] = useState('');
     const [nickname, setNickname] = useState('');
     const [password, setPassword] =useState('');
     const [passwordCheck, setPasswordCheck] = useState('');
     const [term, setTerm] = useState(false);
+    const [PasswordError, setPasswordError] = useState(false);
+    const [termError, setTermError] = useState(false);
 
-
-    
     const onSubmit = (e) => { 
-        e.preventDefault();
+        e.preventDefault(); //이벤트를 취소할 수 있는 경우, 이벤트의 전파를 막지않고 그 이벤트를 취소합니다.
+        if( password !=  passwordCheck ) {
+            return setPasswordError(ture);
+        }
+        if( !term ) {
+            return setTermError(true);
+        }
+        
         console.log({
             id,
             nickname,
@@ -38,11 +44,13 @@ const Signup = () => {
     };
 
     const onChangePasswordCheck = (e) => { 
+        setPasswordError(e.target.value !== password);
         setPasswordCheck(e.target.value);
      };
 
     const onChangeTerm = (e) => {
-        setTerm(e.target.value);
+        setTermError(false);
+        setTerm(e.target.checked);
     };
 
     return (
@@ -74,11 +82,13 @@ const Signup = () => {
                         <label htmlFor = "user-password-check" >비밀번호체크</label>
                         <br />
                         <Input name="user-password-check" type="password" value={passwordCheck} required onChange = {onChangePasswordCheck} />
+                        {PasswordError && <div style= {{ color : 'red'}}> 비밀번호가 일치하지 않습니다.</div>} 
                     </div>
                     <div>
                         <Checkbox name = "user-term" value={term} onChange = {onChangeTerm} > 동의합니다. </Checkbox>
+                        {termError && <div style={{ color : 'red'}}>약관에 동의하셔야 합니다.</div>} 
                     </div>
-                    <div>
+                    <div style={{margintop : 10}}>
                         <Button type="primary" htmlType="submit">가입하기</Button>
                     </div>
                 </Form>                
