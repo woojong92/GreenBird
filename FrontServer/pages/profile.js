8c9104d7d0fb0b77c9b17b5c9b8bbea7e26e826d
Woojong Kim <woojong92@gmail.com>
2019-05-27 12:19:06 +0900

    프로필 화면 만들기

--- FrontServer/pages/profile.js
+++ FrontServer/pages/profile.js
@@ -1,14 +1,45 @@
 import React from 'react';
-import Link from 'next/link';
-import Head from 'next/head';
-import AppLayout from '../components/AppLayout';
+import { Form, List, Button, Input, Card, Icon } from 'antd';
 
 const Profile = () => {
     return (
-        <>
-            <div>Profile</div>
-        </>
+        <div>
+            <Form style = {{ marginBottom: '20px', border: '1px solid #d9d9d9', padding: '20px'}} > 
+                <Input addonBefore = "닉네임" />
+                <Button type ="primary">수정</Button>
+            </Form>
+            
+            <List
+                style = {{ marginBottom : '20px'}}
+                grid = {{gutter: 4, xs: 2, md :3}}
+                size = "small"
+                header = {<div>팔로잉 목록</div>}
+                loadMore = {<Button style = {{ width: '100%'}}>더 보기</Button>}
+                boardered
+                dataSource = {['제로초', '바보', '노드버드오피셜']}
+                renderItem = {item => (
+                    <List.Item style = {{marginTop: '20px'}}>
+                        <Card actions = {[<Icon type ="stop" />]}><Card.Meta description = {item} /></Card>
+                    </List.Item>
+                )}
+            />
+            <List
+                style = {{ marginBottom : '20px'}}
+                grid = {{gutter: 4, xs: 2, md :3}}
+                size = "small"
+                header = {<div>팔로워 목록</div>}
+                loadMore = {<Button style = {{ width: '100%'}}>더 보기</Button>}
+                boardered
+                dataSource = {['제로초', '바보', '노드버드오피셜']}
+                renderItem = {item => (
+                    <List.Item style = {{marginTop: '20px'}}>
+                        <Card actions = {[<Icon type ="stop" />]}><Card.Meta description = {item} /></Card>
+                    </List.Item>
+                )}
+            />
+        </div>
     );
 };
 
 export default Profile;
+
